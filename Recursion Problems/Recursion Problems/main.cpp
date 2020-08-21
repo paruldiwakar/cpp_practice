999fad6329d310e19a6285c4d07a8da4908105df
Parul Diwakar <47185431+paruldiwakar@users.noreply.github.com>
2020-08-21 20:13:38 +0530

    Add files via upload

---
+++ Recursion Problems/Recursion Problems/main.cpp
@@ -0,0 +1,65 @@
+//
+//  main.cpp
+//  Recursion Problems
+//
+//  Created by Parul Diwakar on 13/08/20.
+//  Copyright © 2020 Parul Diwakar. All rights reserved.
+//
+
+#include<iostream>
+using namespace std;
+
+/*void fibonacci(int n){
+     static int n0 = 0, n1 = 1, n2;
+     if (n > 1){
+        n2 = n0 + n1;
+        n0 = n1;
+        n1 = n2;
+        cout << " " << n2 << " " ;
+        fibonacci(n - 1);
+    }
+}
+int main (){
+    int n = 9;
+    cout << 0 << " " << 1;
+    fibonacci(n);
+    return 0;
+}*/
+
+/*#define N 20;
+int n = 1;
+  
+// Prints n, increments n and calls fun1()
+void fun2();
+void fun1()
+{
+    if (n <= N)
+    {
+        printf("%d", n);
+        n++;
+        fun2();
+    }
+    else
+        return;
+}
+  
+// Prints n, increments n and calls fun2()
+void fun2()
+{
+    if (n <= N)
+    {
+        printf("%d", n);
+        n++;
+        fun1();
+    }
+    else
+        return;
+}
+  
+// Driver Program
+int main(void)
+{
+    fun1();
+    return 0;
+}*/
+
